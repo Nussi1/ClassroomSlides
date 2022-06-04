c2e951c242f019e0853e118e3640b0ba30eb50cf
Nussi1 <59906591+Nussi1@users.noreply.github.com>
2022-06-04 23:27:57 +0600

    Add files via upload

---
+++ march_31_json.py
@@ -0,0 +1,15 @@
+# 1
+'''Создайте 3 dictionary c ключами: login, email, age, phone_number. Добавьте их в лист и запишите его в JSON файл. После чего выгрузите все данные из JSON, добавьте к каждой dictionary ключ language и снова запишите в JSON.'''
+
+# import requests
+# r = requests.get('https://api.github.com/emojis').json()
+# print(r)
+
+# 2
+'''Возьмите JSON по https://api.github.com/events, добавьте к каждому элементу {"actor": "Kairat Sexy"} и сохраните у себя на рабочем столе. '''
+
+# json ОБЪЕКТ {"name": <Ваше Имя>,"model":<Модель Вашего Ноутбука>}. 
+
+# import requests
+# data = requests.post(url='https://httpbin.org/post', json={"name": "Kairat Sexy")
+# print(data.json())
