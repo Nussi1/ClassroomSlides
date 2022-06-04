d17d8e7fe8a801dd254129ac0307008fe851cf20
Nussi1 <59906591+Nussi1@users.noreply.github.com>
2022-06-04 11:36:44 +0600

    Add files via upload

---
+++ list_march_14.py
@@ -0,0 +1,43 @@
+# 1
+'''Создать Лист и заполнить его 5 РАЗНЫМИ ТИПАМИ ДАННЫХ.'''
+
+# my_list = [1, 'str', True, [1,2,3], (1,2,3), 'jack', 'jack', 7, 8, 9]
+# print(my_list)
+
+# 2
+'''Взять Лист №1 и посчитать сколько раз там встречается имя "Jack".
+'''
+# names = ['JACK', 'JIMMY', 'JACKSON', 'JHON', 'JACKSON', 'JHON',  'JIMMY', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON',]
+# print(names.count('JACK'))
+
+# 3
+'''Удалить из Листа №1 все чётные индексы до 10.'''
+
+names = ['JACK', 'JIMMY', 'JACKSON', 'JHON', 'JACKSON', 'JHON',  'JIMMY', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON',]
+# del names[0]
+# del names[2]
+# del names[4]
+# del names[6]
+# del names[8]
+# print(names)
+
+# 4
+'''Создать пустой лист.
+Добавить в него сначала Ваше Имя, Год Рождения, любимый Язык Программирования.'''
+
+# my_list = []
+# my_list.append('Nuraika')
+# my_list.append(1988)
+# my_list.append('Python')
+# print(my_list)
+
+# 5
+'''1.Создать Лист из 5 разных имён.
+2.Создать пустую строку и через .join() соеденить пустую строку и все имена в списке.'''
+
+# my_names = ['wdd', 'wfv', 'dcvgfhnhn', 'efwvwv', 'fcedcvvdaa']
+# r = ' '
+# n = r.join(my_names)
+# print(n)
+
+
